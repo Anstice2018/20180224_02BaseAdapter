bbe6d7fce9c90757ca594ebebb267f958f57ecf8
Anstice <s410378047@gm.ntpu.edu.tw>
2018-02-24 17:06:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a20180224_02baseadapter/City.java
@@ -0,0 +1,16 @@
+package com.example.student.a20180224_02baseadapter;
+
+/**
+ * Created by student on 2018/2/24.
+ */
+
+public class City {
+    public String cityname;
+    public String citycode;
+    public int cityimg;
+    public City(String cityname, String citycode, int cityimg) {
+        this.cityname = cityname;
+        this.citycode = citycode;
+        this.cityimg = cityimg;
+    }
+}
---
+++ app/src/main/java/com/example/student/a20180224_02baseadapter/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.student.a20180224_02baseadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    MyAdapter adapter;
+    City cities[] = new City[4];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
+        adapter = new MyAdapter();
+        lv.setAdapter(adapter);
+        cities[0] = new City("台北", "02", R.drawable.tp);
+        cities[1] = new City("台中", "04", R.drawable.tc);
+        cities[2] = new City("台南", "06", R.drawable.tn);
+        cities[3] = new City("高雄", "07", R.drawable.ks);
+
+
+    }
+
+    class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return cities.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+            View v = inflater.inflate(R.layout.item1, null);
+            TextView tv = v.findViewById(R.id.textView);
+            TextView tv2 = v.findViewById(R.id.textView2);
+            ImageView iv = v.findViewById(R.id.imageView);
+            Button btn = v.findViewById(R.id.button);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, cities[position].cityname, Toast.LENGTH_SHORT).show();
+                }
+            });
+            tv.setText(cities[position].cityname );
+            tv2.setText(cities[position].citycode );
+            iv.setImageResource(cities[position].cityimg);
+            return v;
+        }
+    }
+
+}
